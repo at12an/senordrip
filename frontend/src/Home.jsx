8c659a1dd08247c297c29a5c44b4ad64831e2dcd
at12an <ahttran19@gmail.com>
2024-01-12 22:46:20 +1100

    Code Cleanup + Style Change
    Updated colours for about section
    Code cleanup + file organisation

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,20 +1,21 @@
 import './App.css';
-import { Container, AppBar, Typography, Box, Button, ImageList } from '@mui/material';
+import { Container, Typography, Box, Button } from '@mui/material';
 import '@fontsource/roboto';
-import banner from './banner3.png'; 
-import logo from './logo5.png';
+import banner from './images/banner3.png'; 
+import logo from './images/logo5.png';
 import StandardImageList from './ImageList';
 
 
 function Home() {
   return (
     <>
-        <Container sx={{
-            height: '200vh',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-        }}>
+      <Container sx={{
+        height: '200vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+        }}
+      >
         <Box 
           justifyContent='center'
           sx={{
@@ -25,7 +26,8 @@ function Home() {
             alignItems: 'center',
             display: 'flex',
             textAlign: 'center'
-        }}>
+          }}
+        >
           <Box 
             sx={{
               display: 'flex',
@@ -33,38 +35,17 @@ function Home() {
               alignItems: 'center',
               height: '50%',
               justifyContent: 'space-around'
-            }}>
-            {/* <Typography 
-              variant='h2' 
-              sx={{
-                fontFamily: 'Roboto', 
-                color: "white", 
-                fontWeight: 500, 
-                letterSpacing: '0.2rem', 
-                textAlign: 'center'
-                }}>
-                  SENOR DRIP
-            </Typography> */}
+            }}
+          >
+
             <img 
                 src={logo} 
+                alt='logo'
                 style={{
                     height: '300px',
-                    marginBottom: '20px',
-                    // position: 'fixed'
-                }}>
-            </img>
-
-            {/* <Typography 
-              variant='p' 
-              sx={{
-                fontFamily: 'Roboto', 
-                color: "black   ", 
-                fontWeight: 500, 
-                textAlign: 'center',
-                mb: '10px'
-                }}>
-                  Explore top-tier hair services & unlock your tailored hair transformation
-            </Typography> */}
+                    marginBottom: '20px'
+                }}
+            ></img>
 
             <Button 
               variant='contained'
@@ -83,43 +64,72 @@ function Home() {
                     width: '203px',
                     boxShadow: 'none'
                   }
-              }}> 
-                Book Now
+              }}
+            > 
+              Book Now
             </Button>
           </Box>
         </Box>
+
+
         <Box
           sx={{
             height: '75vh',
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center'
-            // flexDirection: 'column'
           }}
         >
-            <Box sx={{mr:'50px'}}>
-                <Typography variant='h1'>
-                DISCOVER SENOR DRIP
-                </Typography>
-            </Box>
-            <Box>
-                <Typography>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.           
-                </Typography>
-                <Typography sx={{mt:'30px'}}>
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                </Typography>
-
-            </Box>
+          <Box sx={{mr:'50px'}}>
+              <Typography 
+                variant='h1'
+                sx={{
+                  color:'#192A56'
+                }}
+              >
+              DISCOVER SENOR DRIP
+              </Typography>
+          </Box>
+          <Box>
+            <Typography 
+              sx={{
+                mt:'30px',
+                color:'#192A56'
+              }}
+            >
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.           
+            </Typography>
+            <Typography 
+              sx={{
+                mt:'30px',
+                color:'#192A56'
+              }}
+            >
+                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            </Typography>
+          </Box>
         </Box>
+
+
         <Box
           sx={{
-            height: '50vh',
+            height: '75vh',
             width: '100vw',
-            bgcolor: 'black'
+            bgcolor: '#192A56',
+            display: 'flex',
+            flexDirection: 'column'
           }}
         >
-            <StandardImageList></StandardImageList>
+          {/* <StandardImageList></StandardImageList> */}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row'
+            }}
+          >
+            
+          </Box>
+          <Box></Box>
         </Box>
       </Container>
     </>
--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -1,82 +1,32 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { AppBar, Box, Toolbar, Typography, Container, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-import logo from './logo3.jpg';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import logo from './images/logo3.jpg';
+import '@fontsource/roboto';
 
 const pages = ['Home', 'Services', 'Bookings', 'Contact'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 function Navigation() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null); 
-  };
 
   return (
     <AppBar position="static" sx={{bgcolor:'#F1F0EF', color:'black', width: '100%', p:1}}>
       <Container maxWidth="xl"> 
         <Toolbar disableGutters sx={{alignItems:'center', alignContent:'center'}}>
-          {/* <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              ml: 15,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'Helvetica',
-              fontWeight: 600,
-              letterSpacing: '.1rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            SENOR DRIP
-          </Typography> */}
           <img 
             src={logo} 
+            alt='logo'
             style={{
               height: '100px',
               marginLeft: '175px'
-            }}>
-
-            </img>
+            }}
+          >
+          </img>
           
           
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
                 sx={{ 
                   my: 2, 
                   display: 'block', 
@@ -85,7 +35,8 @@ function Navigation() {
                   '&:hover': {
                     textDecoration: 'underline',
                   }
-                }}>
+                }}
+              >
                 <Link 
                   to={`/${page}`} 
                   style={{
@@ -97,7 +48,8 @@ function Navigation() {
                     letterSpacing: '.1rem',
                     textTransform: 'none',
                     fontSize: '0.8rem'
-                  }}>
+                  }}
+                >
                   {page}
                 </Link>
               </Button>
@@ -109,7 +61,8 @@ function Navigation() {
               flexGrow: 0,
               border: '2px solid #000000',
               mr: '100px'
-          }}>
+            }}
+          >
             <Typography
               sx={{
                 px: 2,
@@ -117,7 +70,8 @@ function Navigation() {
                 fontWeight: 300,
                 fontFamily: 'Roboto',
                 fontSize: '0.8rem'
-            }}>
+              }}
+            >
               Book Now
             </Typography>
           </Box>
