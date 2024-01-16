d51eb8ec4483828ad9c89bbd95bd8ae5de9737a8
at12an <ahttran19@gmail.com>
2024-01-16 23:37:06 +1100

    finished homepage
    homepage finished

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,17 +1,39 @@
 import './App.css';
 import { Container, Typography, Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import '@fontsource/roboto';
-import banner from './images/banner3.png'; 
-import logo from './images/logo5.png';
-import cut from './images/cut1.jpg'
+import banner from './images/banners/banner3.png'; 
+import logo from './images/logos/logo3.png';
+import cut1 from './images/cuts/cut1.jpg';
+import cut2 from './images/cuts/cut2.jpg';
+import cut3 from './images/cuts/cut3.jpg';
+import cut4 from './images/cuts/cut4.jpg';
+import cut5 from './images/cuts/cut5.jpg';
+import cut6 from './images/cuts/cut6.jpg';
+import insta from './images/icons/instagram.png';
+import email from './images/icons/email.png';
+import phone from './images/icons/phone.png';
+import location from './images/icons/location.png';
+
+
 // import StandardImageList from './ImageList';
 
 
 function Home() {
-  return (
+
+  const navigate = useNavigate();
+
+  const move = () => {
+    navigate('/bookings');
+  }
+
+  const redirect = () => {
+    window.location.href = 'https://www.instagram.com/senordrip/';
+  };
+
+  return (  
     <>
       <Container sx={{
-        height: '200vh',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center'
@@ -20,7 +42,7 @@ function Home() {
         <Box 
           justifyContent='center'
           sx={{
-            height: '80vh',
+            height: '70vh',
             width: '100vw',
             bgcolor: '#192A56',
             backgroundImage: `url(${banner})`,
@@ -39,16 +61,18 @@ function Home() {
             }}
           >
 
-            <img 
+            <img
+                id='home-banner'
                 src={logo} 
                 alt='logo'
                 style={{
-                    height: '300px',
-                    marginBottom: '20px'
+                    height: '250px',
+                    marginBottom: '30px'
                 }}
             ></img>
 
             <Button 
+              onClick={move}
               variant='contained'
               sx={{
                 width: '200px',
@@ -56,13 +80,13 @@ function Home() {
                 color: 'black',
                 bgcolor: '#F1F0EF',
                 boxShadow: 'none',
-                transition: 'width 0.25s, height 0.25s',
+                transition: '0.75s',
                 fontWeight: 500,
                 letterSpacing: '0.1rem',
                 fontFamily: 'Roboto',
                 '&:hover': {
-                    backgroundColor: '#F1F0EF',
-                    width: '203px',
+                    bgcolor: 'black',
+                    color: 'white',
                     boxShadow: 'none'
                   }
               }}
@@ -74,28 +98,43 @@ function Home() {
 
 
         <Box
+          id='home-about-box'
           sx={{
-            height: '75vh',
+            width: '70vw',
             display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center'
+            justifyContent: 'center',
+            alignItems: 'center',
+            py: 10
           }}
         >
-          <Box sx={{mr:'50px'}}>
-              <Typography 
-                variant='h1'
-                sx={{
-                  color:'#192A56'
-                }}
-              >
+          <Box
+            className='home-about-textbox' 
+            sx={{ 
+              width: '50%'
+            }}
+          >
+            <Typography 
+              id='home-about-h1'
+              variant='h1'
+              sx={{
+                color:'black',
+                pr: 2
+              }}
+            >
               DISCOVER SENOR DRIP
-              </Typography>
+            </Typography>
           </Box>
-          <Box>
+          <Box 
+            className='home-about-textbox' 
+            sx={{
+              p: 1,
+              width: '50%'
+            }}
+          >
             <Typography 
               sx={{
                 mt:'30px',
-                color:'#192A56'
+                color:'black'
               }}
             >
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.           
@@ -114,70 +153,134 @@ function Home() {
 
         <Box
           sx={{
-            height: '70vh',
+            // height: '70vh',
             width: '100vw',
             bgcolor: '#192A56',
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'space-evenly'
+            justifyContent: 'space-evenly',
+            alignItems: 'center'
           }}
         >
-          {/* <StandardImageList></StandardImageList> */}
           <Box
+            className='home-picture-box'
             sx={{
               display: 'flex',
+              width: '66vw',
               flexDirection: 'row',
-              justifyContent: 'space-evenly'
+              justifyContent: 'space-evenly',
+              alignItems: 'center'
             }}
           >
             <img 
-              src={cut} 
-              alt='cut'
+              src={cut1} 
+              alt='cut1'
               style={{
-                  height: '35vh'
+                width: '22vw'
               }}
             ></img>
             <img 
-              src={cut} 
-              alt='cut'
+              src={cut2} 
+              alt='cut2'
               style={{
-                height: '35vh'
+                width: '22vw'
               }}
             ></img>
             <img 
-              src={cut} 
-              alt='cut'
+              src={cut3} 
+              alt='cut3'
               style={{
-                height: '35vh'
+                width: '22vw'
               }}
             ></img>
           </Box>
           <Box
+            className='home-picture-box'
             sx={{
               display: 'flex',
+              width: '66vw',
               flexDirection: 'row',
-              justifyContent: 'space-evenly'
+              justifyContent: 'space-evenly',
+              alignItems: 'center'
+              // pb: .5
             }}
           >
             <img 
-              src={cut} 
-              alt='cut'
+              src={cut4} 
+              alt='cut4'
               style={{
-                height: '35vh'
+                width: '22vw'
+              }}
+            ></img> 
+            <img 
+              src={cut5} 
+              alt='cut5'
+              style={{
+                width: '22vw'
+              }}
+            ></img>
+            <img
+              src={cut6} 
+              alt='cut6'
+              style={{
+                width: '22vw'
+              }}
+            ></img>
+          </Box>
+        </Box>
+        
+
+        <Box
+          sx={{
+            height:'20vh',
+            width: '100vw',
+            bgcolor: '#F1F0EF',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-evenly'
+          }}
+        >
+          <Box
+            id='footer-icon-box'
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-evenly',
+              width: '25%'
+            }}
+          >
+            <img 
+              onClick={redirect}
+              className='footer-icon'
+            
+              src={insta} 
+              alt='instagram'
+              style={{
+                width: '30px'
               }}
             ></img>
             <img 
-              src={cut} 
-              alt='cut'
+              className='footer-icon'
+              src={phone} 
+              alt='phone'
               style={{
-                height: '35vh'
+                width: '30px'
               }}
             ></img>
             <img 
-              src={cut} 
-              alt='cut'
+              className='footer-icon'
+              src={location} 
+              alt='location'
               style={{
-                height: '35vh'
+                width: '30px'
+              }}
+            ></img>
+            <img 
+              className='footer-icon'
+              src={email} 
+              alt='email'
+              style={{
+                width: '30px'
               }}
             ></img>
           </Box>
--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
 import { AppBar, Box, Toolbar, Typography, Container, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import logo from './images/logo3.jpg';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from './images/logos/logo1-bg.jpg';
 import '@fontsource/roboto';
 
 const pages = ['Home', 'Services', 'Bookings', 'Contact'];
 
 function Navigation() {
 
+  const navigate = useNavigate();
+
+  const move = () => {
+    navigate('/bookings');
+  }
+ 
   return (
     <AppBar position="static" sx={{bgcolor:'#F1F0EF', color:'black', width: '100%', p:1}}>
       <Container maxWidth="xl"> 
-        <Toolbar disableGutters sx={{alignItems:'center', alignContent:'center'}}>
+        <Toolbar id='toolbar' disableGutters sx={{alignItems:'center', alignContent:'center'}}>
           <img 
+            id='nav-logo'
             src={logo} 
             alt='logo'
             style={{
               height: '100px',
-              marginLeft: '175px'
+              marginLeft: '175px',
+              marginRight: '10px  '
             }}
           >
           </img>
@@ -26,28 +34,32 @@ function Navigation() {
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
+                disableRipple
                 key={page}
                 sx={{ 
                   my: 2, 
                   display: 'block', 
                   color: 'black',
                   mx: 1.5,
+                  transition: '0.5s',
                   '&:hover': {
                     textDecoration: 'underline',
+                    backgroundColor: 'transparent'
                   }
                 }}
               >
                 <Link 
+                  className='nav-link'
                   to={`/${page}`} 
                   style={{
                     textDecoration: 'none',
-                    color: 'black',
+                    color: 'grey',
                     paddingBottom: '10px',
                     fontFamily: 'Roboto',
                     fontWeight: 300,
                     letterSpacing: '.1rem',
                     textTransform: 'none',
-                    fontSize: '0.8rem'
+                    fontSize: '0.8rem',
                   }}
                 >
                   {page}
@@ -57,10 +69,17 @@ function Navigation() {
           </Box>
 
           <Box 
+            onClick={move}
+            id='nav-book-button'
             sx={{ 
               flexGrow: 0,
-              border: '2px solid #000000',
-              mr: '100px'
+              border: '1px solid #000000',
+              mr: '150px',
+              transition: '0.75s',
+              '&:hover': {
+                backgroundColor: 'black',
+                color: 'white'
+              }
             }}
           >
             <Typography
